a21e0ba2b141f1f27602b8b02fb9b5d24fbb9dc8
Felipe Mamede <felipebrasilmv@hotmail.com>
2020-08-03 18:46:01 -0300

    Fix bugs

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -3,28 +3,23 @@ import { Link } from 'react-router-dom'
 
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
+
 import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import PurpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
 import './styles.css'
+
 function Landing() {
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
         <div className="logo-container">
-          <img 
-            src={logoImg} 
-            alt="Proffy"
-          />
+          <img src={logoImg} alt="Proffy" />
           <h2>Sua plataforma de estudos online.</h2>
         </div>
 
-        <img 
-          src={landingImg} 
-          alt="Plataforma de estudos" 
-          className="hero-img"
-        />
+        <img src={landingImg} alt="Plataforma de estudos" className="hero-image" />
 
         <div className="buttons-container">
           <Link to="study" className="study">
@@ -37,7 +32,8 @@ function Landing() {
           </Link>
         </div>
         <span className="total-connections">
-          Total de 200 conexões já realizadas! <img src={PurpleHeartIcon} alt="Coração roxo"/>
+          Total de 200 conexões já realizadas! 
+          <img src={PurpleHeartIcon} alt="Coração roxo"/>
         </span>
       </div>
     </div>
